b4e89d83afaa6d0d40fe65c7f87a8a6efe7dc089
Michael Knopacki <michaelknopacki@hotmail.com>
2026-01-11 08:38:36 -0300

    20260111(1)

--- 1.8 Lista de exercicios.php
+++ 1.8 Lista de exercicios.php
@@ -27,32 +27,37 @@ echo teste($n1,$n2,$operacao)."\n";*/
 
 // 2. Crie uma função em PHP que calcule o IMC baseado na altura e peso passados por parâmetro.
 
-// $peso = 120;
-// $altura = 1.80;
-// $imc = 0;
+ $peso = 120;
+ $altura = 1.80;
 
-// $imc = $peso / ($altura * $altura);
+ function calculaImc ($peso, $altura){
 
-// if ( $imc < 18.5 ){
-// 	echo "abaixo do peso";
-// } else if ( $imc >= 18.5 && $imc <= 24.9) {
-// 	echo "Peso normal";
-// } else if ( $imc >= 25 && $imc <= 29.9) {
-// 	echo "Sobrepeso";
-// } else if ( $imc >= 30 && $imc <= 34.9) {
-// 	echo "Obesidade grau 1";
-// } else if ( $imc >= 35 && $imc <= 39.9) {
-// 	echo "Obesidade grau 2";
-// } else if ( $imc >= 40 ) {
-// 	echo "Obesidade grau 3";
-// }
+    $imc = $peso / ($altura * $altura);
+
+    if ( $imc < 18.5 ){
+        return "abaixo do peso";
+    } else if ( $imc >= 18.5 && $imc <= 24.9) {
+        return "Peso normal";
+    } else if ( $imc >= 25 && $imc <= 29.9) {
+        return "Sobrepeso";
+    } else if ( $imc >= 30 && $imc <= 34.9) {
+        return "Obesidade grau 1";
+    } else if ( $imc >= 35 && $imc <= 39.9) {
+        return "Obesidade grau 2";
+    } else if ( $imc >= 40 ) {
+        return "Obesidade grau 3";
+    }
+
+}
+
+echo calculaImc ($peso, $altura)."\n";
 
 // 3. Crie uma função em PHP que converta graus celsius para Fahrenheit.
 
 
 // Desafio extra - chatGPT
 
-$n1 = 10;
+/*$n1 = 10;
 
 function parOuImpar ($n1){
 
@@ -63,6 +68,6 @@ function parOuImpar ($n1){
     }
 }
 
-echo parOuImpar($n1)."\n";
+echo parOuImpar($n1)."\n";*/
 
 ?>
